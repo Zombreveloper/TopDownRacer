f692cd1ed18a2f9f93917d4fa0e4f51488d83a7f
Lazer <98yoda@gmx.de>
2022-05-16 11:26:00 +0200

    ScriptableObject
    Has Folder to Create from and has a first very simple scriptable object.

---
+++ Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerProfile", menuName = "ScriptableObject/PlayerProfile")]
+public class PlayerProfile : ScriptableObject
+{
+
+    public string myString;
+}
