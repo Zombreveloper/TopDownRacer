806fc3e1033c2224fe31c5a34ded51495e1cf960
Marvin <ma.stickan@gmail.com>
2022-05-23 13:25:54 +0200

    Renamed Race Track Scene, barebones working BuildManager

--- Assets/Scripts/Map and Tiling/MapBuilder.cs
+++ Assets/Scripts/Map and Tiling/MapBuilder.cs
@@ -17,44 +17,79 @@ public class MapBuilder : MonoBehaviour
 	
 	public GameObject straightTrackPrefab;
 	
+	//Vectors for positions on a grid to build on
 	private Vector3Int buildPos;
 	private Vector3 prefabSpawnPoint;
+
+
 	private Tile _straightTrack;
+
+	//private IEnumerator TilingDelay;
 	
 	
     // Start is called before the first frame update
     void Start()
     {
+
+		StartCoroutine(TilingDelay());
+
+	}
+
+
+
+
+	// Update is called once per frame
+	void Update()
+    {
+		PlaceTileByMouse();
 		
-		//builds the street Tiles for now. going to be outsourced
-		for (int i=-2; i<2; i++)
-		{
-			buildPos.Set(i,1,0);
-			PlaceSingleTile(buildPos);
-			
-		}
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+	//Coroutine that waits for a given time to set a tile
+	private IEnumerator TilingDelay()
+	{
+		int currentHeight;
+		currentHeight = 1;
+
+		Debug.Log("Started Coroutine at timestamp : " + Time.time);
+		for (currentHeight = -1; currentHeight < 10; currentHeight++)
+		{
+			StraightLinePattern(currentHeight);
+			yield return new WaitForSeconds(1);
+		}
+
+		Debug.Log("Ended Coroutine at timestamp : " + Time.time);
+	}
+
+	void StraightLinePattern(int posY)
+	{
+		for (int i = -1; i < 2; i++)
+		{
+			buildPos.Set(i, posY, 0);
+			PlaceSingleTile(buildPos);
+		}	
+	}
+
+	
+	void PlaceSingleTile(Vector3Int pos) //places a tile on a given position on the grid
+	{
+		currentTilemap.SetTile(pos, currentTile);
+	}
+
+	
+	void PlaceTileByMouse()
+	{
 		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 		Vector3Int pos = currentTilemap.WorldToCell(mousePosition);
-		
-		
+
+
 		if (Input.GetMouseButton(0))
 		{
 			PlaceSingleTile(pos);
 		}
-    }
-	
-	//places a tile on a given position on the grid
-	void PlaceSingleTile(Vector3Int pos)
-	{
-		currentTilemap.SetTile(pos, currentTile);
-	}
-	
-	//Intanciates a Prefab on given positions on the grid
+	} //Click anywhere to place a given tile set by the currentTile
+
+
 	void PlacePrefab(Vector3Int pos)
 	{
 		prefabSpawnPoint = currentGrid.CellToWorld(pos);
@@ -63,5 +98,5 @@ public class MapBuilder : MonoBehaviour
 		//currentTilemap.SetTile(buildPos, straightTrackPrefab);
 		//_straightTrack = Instantiate(straightTrackPrefab);
 		//currentTilemap.SetTile(buildPos, _straightTrack);
-	}
+	}//MOST LIKELY NOT EVER USED!!  Instanciates a Prefab on given positions on the grid
 }
