5e3d54073fa3ce4867b32c27c23093c1ac5e2c08
Marvin <ma.stickan@gmail.com>
2022-08-18 16:45:50 +0200

    start grid update
    - takes freely placable  reference point
    - can theoretically place an infinite amount of cars
    ...seriously try it. It`s hilarious!

--- Assets/Scripts/ParticipantsManager.cs
+++ Assets/Scripts/ParticipantsManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class ParticipantsManager : MonoBehaviour
@@ -16,10 +17,16 @@ public class ParticipantsManager : MonoBehaviour
     public GameObject bumperPrefab;
     private GameObject currentBumper;
 
+    private GameObject startPointObject; //GameObject in a level to determine where the grid of cars spawns (optional)
+
     void Awake()
     {
         index = 1;
         activeCars.carsList.Clear();
+
+        startPointObject = GameObject.Find("/LevelStartPoint");
+        if (startPointObject != null)
+            Debug.Log("I found a startpoint!");
         spawnCars();
     }
 
@@ -89,31 +96,75 @@ public class ParticipantsManager : MonoBehaviour
         }
     }
 
-    Vector2 positionCar()
+    //positions participating cars on a formula1 like starting grid. Propably better off as an own class but whatever
+    //TODO: add in the reference Points rotation if necessary
+    Vector2 positionCar() 
     {
-        int xDistance = 6; //vertical distance between cars in start Grid
-        int yDistance = 3; //horizontal distance in start Grid
-        int carsInRow = 4;
-        float setBackRow = xDistance * carsInRow; //widht of the row on the starting grid
-
-        if (index <= carsInRow) //first 4 cars
+        int _lowIndex = index - 1; //to let Index start at 0. Important for Division
+        Vector2 _referencePoint = Vector2.zero;
+        if (startPointObject != null)
         {
-            float x = xDistance * index;
-            float y = yDistance * -index;
-            return new Vector2(x, y);
+           _referencePoint = startPointObject.transform.position;
+            //Debug.Log("LevelStartPoint is on " + _startPoint);
         }
-        else if (index > carsInRow && index <= (carsInRow * 2)) //other 4 cars
-        {            
-            float x = (xDistance * index) - setBackRow;
-            float y = yDistance * -index;
-            return new Vector2(x, y);
+        int _xDistance = 6; //vertical distance between cars in start Grid
+        int _yDistance = 3; //horizontal distance in start Grid
+        int _carsInRow = 4;
+        float _rowWidth = _xDistance * (_carsInRow-1);
+
+        int _placeInRow = _lowIndex % _carsInRow; //gives a value between 0 and the amount of cars to be set until the row resets
+
+        //calculations start here
+            float x = (_xDistance * _placeInRow);
+            float y = _yDistance * -index;
+            Vector2 _spawnPoint = new Vector2(x, y) + _referencePoint;
+            _spawnPoint = _spawnPoint - new Vector2(_rowWidth / 2, 0); //offsets the LevelStartPoint to the center of the row
+            return _spawnPoint;
+    }
+
+
+    // trash dump
+    Vector2 positionCarBackup()
+    {
+        Vector2 _startPoint = Vector2.zero;
+        if (startPointObject != null)
+        {
+            _startPoint = startPointObject.transform.position;
+            Debug.Log("LevelStartPoint is on " + _startPoint);
+        }
+        int _xDistance = 6; //vertical distance between cars in start Grid
+        int _yDistance = 3; //horizontal distance in start Grid
+        int _carsInRow = 4;
+        float _setBackRow = _xDistance * _carsInRow; //width of the row on the starting grid
+
+        if (index <= _carsInRow) //first 4 cars
+        {
+            float x = _xDistance * index;
+            float y = _yDistance * -index;
+            Vector2 _spawnPoint = new Vector2(x, y) + _startPoint;
+            return _spawnPoint;
+        }
+        else if (index > _carsInRow && index <= (_carsInRow * 2)) //other 4 cars
+        {
+            float x = (_xDistance * index) - _setBackRow;
+            float y = _yDistance * -index;
+            Vector2 _spawnPoint = new Vector2(x, y) + _startPoint;
+            return _spawnPoint;
         }
         else //if for whatever reason there would be more than 8 cars on the track
         {
-            float x = (xDistance * index) - 2*setBackRow;
-            float y = yDistance * -index;
-            return new Vector2(x, y);
+            float x = (_xDistance * index) - 2 * _setBackRow;
+            float y = _yDistance * -index;
+            Vector2 _spawnPoint = new Vector2(x, y) + _startPoint;
+            return _spawnPoint;
         }
+        /* //here goes pseudo code
+        {
+            float x = (_xDistance * [Rest aus Division index/_cars in row]) - [index / _cars in row, Ergebnis in ganzer Zahl!!] * _setBackRow;
+            float y = _yDistance * -index;
+            Vector2 _spawnPoint = new Vector2(x, y) + _startPoint;
+            return _spawnPoint;
+        }*/
 
     }
 }
