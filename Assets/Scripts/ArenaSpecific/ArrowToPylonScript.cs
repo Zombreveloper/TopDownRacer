8fa62b891708a024043fe389c1987900047702b5
Lazer <98yoda@gmx.de>
2022-08-30 13:11:41 +0200

    Arena and Menu Updates
    Arena Race now has a nearly functioning arrow, and Menu has a bigger background.

--- Assets/Scripts/ArenaSpecific/ArrowToPylonScript.cs
+++ Assets/Scripts/ArenaSpecific/ArrowToPylonScript.cs
@@ -6,20 +6,17 @@ public class ArrowToPylonScript : MonoBehaviour
 {
     Vector3 camCenterPos;
     Vector3 pylonPos;
-    Vector3 topLeftCamCorner;
-    Vector3 topRightCamCorner;
-    Vector3 botLeftCamCorner;
-    Vector3 botRightCamCorner;
 
     Vector3 pylonDir;
 
-    public GameObject ArrowTurntable; //has position and rotation like arrow, is in the middle of the screen and points at the pylon
-    public GameObject ArrowBase; //changes its position.y component relative to the srcreen size
+    public Sprite arrowSprite;
+    public Sprite targetSprite;
+    SpriteRenderer myRenderer;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        myRenderer = GetComponentInChildren<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -31,29 +28,11 @@ public class ArrowToPylonScript : MonoBehaviour
     public void UpdateArrow(GameObject currentWayPoint)
     {
         pylonPos = currentWayPoint.transform.position;
-        UpdateRotation();
         UpdatePosition();
     }
 
     void UpdateRotation()
     {
-        /*
-        //mk1
-
-        //get positions
-        Vector2 pylonPos = currentWayPoint.transform.position;
-        //Vector2 camCenterPos = Camera.main.transform.position;
-
-        //calculate rotation
-        float angle = Vector2.Angle(camCenterPos, pylonPos);
-        Vector3 arrowRot = new Vector3(0.0f, 0.0f, angle); //the angle is completely wrong
-
-        //set rotation
-        Quaternion arrowQ = Quaternion.Euler(arrowRot);
-        transform.rotation = arrowQ;
-        */
-
-
         //mk2
         /*
         Inspired by @abar s answer here;
@@ -71,72 +50,43 @@ public class ArrowToPylonScript : MonoBehaviour
         transform.up = arrowRot;
         //Debug.Log("trafo up: " + arrowRot);
 
-        //
-        //ArrowTurntable Rotation
-        ArrowTurntable.transform.up = arrowRot;
     }
 
     void UpdatePosition()
     {
         //arrow in screen center
-        transform.position = camCenterPos;
-        ArrowTurntable.transform.position = camCenterPos;
+        //transform.position = camCenterPos;
 
-        //
-        //ArrowBase position
-        float turntableRotAngel = ArrowTurntable.transform.rotation.z;
-        //Debug.Log("turntableRotAngel: " + turntableRotAngel);
-        //turntableRotAngel is between 180 and -180, up is 0/-0, down is 180/-180
+        /*
+        Inspired by / copied from Code Monkey, link to the Video here;
+        https://youtu.be/dHzeHh-3bp4
+        */
+        float borderSize = 45f;
+        Vector3 targetPositionScreenPoint = Camera.main.WorldToScreenPoint(pylonPos);
+        bool isOffScreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize || targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize;
 
-        Vector3 arrowBasePos = ArrowBase.transform.position; //get whole position vector
+        if (isOffScreen)
+        {
+            UpdateRotation();
+            myRenderer.sprite = arrowSprite;
 
-        if (-45 <= turntableRotAngel && turntableRotAngel <= 0 || 0 <= turntableRotAngel && turntableRotAngel <= 45 ) //top
-        {
-            //relativeToScreenHeight(arrowBasePos);
-            arrowBasePos.y = ((Camera.main.orthographicSize/2)-1); //change the y component
-            ArrowBase.transform.position = arrowBasePos;
-        }
-        else if (135 <= turntableRotAngel && turntableRotAngel <= 180 || -180 <= turntableRotAngel && turntableRotAngel <= -135) //bot
-        {
-            //relativeToScreenHeight(arrowBasePos);
-            arrowBasePos.y = -((Camera.main.orthographicSize/2)-1); //change the y component
-            ArrowBase.transform.position = arrowBasePos;
-        }
-        else if (45 <= turntableRotAngel && turntableRotAngel <= 135 ) //left
-        {
-            //relativeToScreenWidth(arrowBasePos);
-            float myAspect = Camera.main.aspect;
-            arrowBasePos.y = (((Camera.main.orthographicSize * myAspect)/2)-1); //change the y component
-            ArrowBase.transform.position = arrowBasePos;
-        }
-        else if ( -45 <= turntableRotAngel && turntableRotAngel <= -135) //right
-        {
-            //relativeToScreenWidth(arrowBasePos);
-            float myAspect = Camera.main.aspect;
-            arrowBasePos.y = (((Camera.main.orthographicSize * myAspect)/2)-1); //change the y component
-            ArrowBase.transform.position = arrowBasePos;
+            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
+
+            if (cappedTargetScreenPosition.x <= borderSize) cappedTargetScreenPosition.x = borderSize;
+            if (cappedTargetScreenPosition.x >= Screen.width - borderSize) cappedTargetScreenPosition.x = Screen.width - borderSize;
+            if (cappedTargetScreenPosition.y <= borderSize) cappedTargetScreenPosition.y = borderSize;
+            if (cappedTargetScreenPosition.y >= Screen.height - borderSize) cappedTargetScreenPosition.y = Screen.height - borderSize;
+
+            Vector3 pointerWorldPosition = Camera.main.ScreenToWorldPoint(cappedTargetScreenPosition);
+            transform.position = pointerWorldPosition;
         }
         else
         {
-            Debug.Log("ERROR: _Arrow To Pylon Script_ has no correct angle _Line 98_");
+            transform.up = Vector3.zero;
+            myRenderer.sprite = targetSprite;
+
+            Vector3 pointerWorldPosition = Camera.main.ScreenToWorldPoint(targetPositionScreenPoint);
+            transform.position = pointerWorldPosition;
         }
     }
-
-    /*
-    void relativeToScreenHeight(Vector3 arrowBasePos)
-    {
-        //float arrowBaseY = ((Screen.height/2)-1);
-        //ArrowBase.transform.position.y = arrowBaseY;
-
-        arrowBasePos.y = ((Camera.main.orthographicSize/2)-1); //change the y component
-        ArrowBase.transform.position = arrowBasePos;
-    }
-
-    void relativeToScreenWidth(Vector3 arrowBasePos)
-    {
-        float myAspect = Camera.main.aspect;
-        arrowBasePos.y = (((Camera.main.orthographicSize * myAspect)/2)-1); //change the y component
-        ArrowBase.transform.position = arrowBasePos;
-    }
-    */
 }
