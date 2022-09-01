d7b7b51221691e71e6e3d3bb320f8821166b92aa
Marvin <ma.stickan@gmail.com>
2022-09-01 17:30:42 +0200

    simplification of explosionScript

--- Assets/Scripts/Visual FX/ExplosionScript.cs
+++ Assets/Scripts/Visual FX/ExplosionScript.cs
@@ -8,17 +8,12 @@ public class ExplosionScript : MonoBehaviour
 {
     Vector3 camCenterPos;
     Vector3 pylonPos;
-    Vector3 pointOfImpact;
-
-    Vector3 pylonDir;
-
-    SpriteRenderer myRenderer;
+    Vector3 pointOfImpactScreenSpace;
 
     // Start is called before the first frame update
     void Start()
     {
-        //myRenderer = GetComponentInChildren<SpriteRenderer>();
-        pointOfImpact = this.gameObject.transform.position - Camera.main.transform.position;
+        pointOfImpactScreenSpace = this.gameObject.transform.position - Camera.main.transform.position;
 
     }
 
@@ -28,12 +23,6 @@ public class ExplosionScript : MonoBehaviour
         camCenterPos = Camera.main.transform.position;
         UpdateRotation();
         simpleUpdatePosition(); //currently handled by OutOfScreenDestroyEffects
-    }
-
-    public void UpdateArrow(GameObject currentWayPoint)
-    {
-        pylonPos = currentWayPoint.transform.position;
-        UpdatePosition();
     }
 
     void UpdateRotation()
@@ -46,9 +35,6 @@ public class ExplosionScript : MonoBehaviour
         basically a "LookAt 2d" function
         */
 
-
-        //get positions
-
         //calculate rotation towards CameraCenterPoint
         Vector3 spriteRot = camCenterPos - this.gameObject.transform.position;
         //set rotation
@@ -58,43 +44,8 @@ public class ExplosionScript : MonoBehaviour
     }
 
     void simpleUpdatePosition()
-    {
-        this.gameObject.transform.position = pointOfImpact + camCenterPos;
-    }
-
-    void UpdatePosition()
-    {
-        /*
-        Inspired by / copied from Code Monkey, link to the Video here;
-        https://youtu.be/dHzeHh-3bp4
-        */
-        //float borderSize = 50f;
-        float borderSize = ((-(Camera.main.orthographicSize)) / 2) + 75;
-
-
-        Vector3 targetPositionScreenPoint = camCenterPos;
-        bool isOffScreen = targetPositionScreenPoint.x <= borderSize || targetPositionScreenPoint.x >= Screen.width - borderSize || targetPositionScreenPoint.y <= borderSize || targetPositionScreenPoint.y >= Screen.height - borderSize;
-
-        if (!isOffScreen)
-        {
-            //UpdateRotation();
-
-            Vector3 cappedTargetScreenPosition = targetPositionScreenPoint;
-
-            if (cappedTargetScreenPosition.x <= borderSize) cappedTargetScreenPosition.x = borderSize;
-            if (cappedTargetScreenPosition.x >= Screen.width - borderSize) cappedTargetScreenPosition.x = Screen.width - borderSize;
-            if (cappedTargetScreenPosition.y <= borderSize) cappedTargetScreenPosition.y = borderSize;
-            if (cappedTargetScreenPosition.y >= Screen.height - borderSize) cappedTargetScreenPosition.y = Screen.height - borderSize;
-
-            Vector3 pointerWorldPosition = Camera.main.ScreenToWorldPoint(cappedTargetScreenPosition);
-            transform.position = pointerWorldPosition;
-        }
-        else
-        {
-            transform.up = Vector3.zero;
-
-            Vector3 pointerWorldPosition = Camera.main.ScreenToWorldPoint(targetPositionScreenPoint);
-            transform.position = pointerWorldPosition;
-        }
+    {   
+        Vector3 worldPosition = pointOfImpactScreenSpace + camCenterPos;
+        this.gameObject.transform.position = worldPosition;
     }
 }
