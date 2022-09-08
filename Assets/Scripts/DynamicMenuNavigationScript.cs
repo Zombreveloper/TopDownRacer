d570ce0229fcd5ff9ecbb5420a78801bfb8451a6
Lazer <98yoda@gmx.de>
2022-09-08 15:38:00 +0200

    Update Player Counter Menu
    PlayerCounter Menu can now be navigatet through with only keyboard arrow up and down
    
    the selsction does not jump to the next inputfield onChangeValue because the PlayerCounterMenu Script updates its ui to slow, so either the selection jumps to the startButten (wich would be wrong) or it jumps to the next inputfield, but somehow you cant wroght into it...
    
    better than before.

---
+++ Assets/Scripts/DynamicMenuNavigationScript.cs
@@ -0,0 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+/*
+This Script is designed, to make it easy to navigate throug a Menu of UI elemnts that get dynamicly SetActive or inactive.
+It is used to navigate using ARROW UP AND DOWN.
+It also loops on the vertical axis.
+*/
+
+public class DynamicMenuNavigationScript : MonoBehaviour
+{
+    public List<Selectable> ListOfUiElements = new List<Selectable>(); //has to be filleed in Inspector in Unity with UI Elements from screen-Top to screen-Bottom
+    private int index;
+    private Selectable currentUIElement;
+    private bool downwards = true;
+    public int startIndex; //defines wich object of the ListOfUiElements is the first selected ui Element
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = startIndex;
+        SelectUiElement();
+        //Debug.Log("The current game object is: " + currentUIElement.gameObject.transform.parent.gameObject.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            bool active = true;
+            SelectNorthUiElement(active);
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            bool active = true;
+            SelectSouthUiElement(active);
+        }
+    }
+
+    public void SelectSouthUiElement(bool _active) //cycles through Selectebles from screen-top to screen-bottom
+    {
+        StartCoroutine(waitForOneFrame()); //specific for PlayerCounterMenu, onValueChanged in TMP_InputField gets called before PlayerCounterMenu updates its ui, wich kind of generates an error
+        index++; //next element in list
+
+        if (index >= ListOfUiElements.Count)
+        {
+            index = 0; //loop from bottom to top
+        }
+
+        downwards = true;
+
+        if (_active) //if element has to be SetActive(true)
+        {
+            SelectActiveUiElement();
+        }
+        else if (!_active) //if element has to be SetActive(false) or SetActive doesnt matter
+        {
+            SelectUiElement();
+        }
+    }
+
+    public void SelectNorthUiElement(bool _active)  //cycles through Selectebles from screen-bottom to screen-top
+    {
+        StartCoroutine(waitForOneFrame()); //specific for PlayerCounterMenu, onValueChanged in TMP_InputField gets called before PlayerCounterMenu updates its ui, wich kind of generates an error
+        index--;
+
+        if (index < 0)
+        {
+            index = ListOfUiElements.Count - 1; //loop from top to bottom
+        }
+
+        downwards = false;
+
+        if (_active) //if element has to be SetActive(true)
+        {
+            SelectActiveUiElement();
+        }
+        else if (!_active) //if element has to be SetActive(false) or SetActive doesnt matter
+        {
+            SelectUiElement();
+        }
+    }
+
+    private void SelectActiveUiElement() //selects next UI Element that is SetActive(true)
+    {
+        currentUIElement = ListOfUiElements[index];
+
+        if (currentUIElement.gameObject.transform.parent.gameObject.activeSelf)
+        {
+            currentUIElement.Select();
+        }
+        else if (!currentUIElement.gameObject.transform.parent.gameObject.activeSelf)
+        {
+            if (downwards == true)
+            {
+                bool active = true;
+                SelectSouthUiElement(active);
+            }
+            else if (downwards == false)
+            {
+                bool active = true;
+                SelectNorthUiElement(active);
+            }
+        }
+    }
+
+    private void SelectUiElement() //selects next UI Element
+    {
+        currentUIElement = ListOfUiElements[index];
+        currentUIElement.Select();
+    }
+
+    private IEnumerator waitForOneFrame()
+    {
+        //returning 0 will make it wait 1 frame
+        yield return 0;
+        //code goes here
+    }
+}
